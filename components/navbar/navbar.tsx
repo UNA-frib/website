b061756868a3838ac99202b43fe91a15d510af3f
quentin <brecq1@bfh.ch>
2026-05-15 03:09:32 +0200

    added logo to navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from './navbar.module.css';
 
 export function Navbar() {
@@ -6,6 +7,15 @@ export function Navbar() {
     <nav className={styles.navbar}>
       <Link href="/">Home</Link>
       <Link href="/posts">Posts</Link>
+      <Link href="/" className={styles.logoContainer}>
+        <Image
+          src="/images/una_logo.png"
+          alt="UNA Logo"
+          width={60}
+          height={60}
+          className={styles.logo}
+        />
+      </Link>
       <Link href="/members">Members</Link>
       <Link href="/about">About</Link>
     </nav>
